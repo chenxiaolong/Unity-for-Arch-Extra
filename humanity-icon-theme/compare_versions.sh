8cec5c8e0b9463eaf0d55d29be806e33ad1cff27
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-03-09 19:19:20 -0500

    Add version comparison script for humanity-icon-theme

---
+++ humanity-icon-theme/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/humanity-icon-theme' -O - | sed -n 's/.*>humanity-icon-theme_\(.*\)\.tar\.gz<.*/\1/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q 'https://launchpad.net/humanity/+download' -O - | sed -n 's/.*[Hh]umanity-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
