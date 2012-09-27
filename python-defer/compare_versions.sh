edf64b96fc45b567f8da1d6a1446ba904a3e6ee0
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-27 13:10:13 -0400

    [python-defer] Update to version 1.0.6

--- python-defer/compare_versions.sh
+++ python-defer/compare_versions.sh
@@ -3,13 +3,13 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/python-defer' -O - | sed -n 's/.*>python-defer_\(.*\)-.*\.tar\.gz<.*/\1/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/python-defer' -O - | sed -n 's/.*>python-defer_\(.*\)-.*\.tar\.gz<.*/\1/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/python-defer/+download' -O - | sed -n 's/.*defer-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
 
 echo ""
 
-echo -e "PKGBUILD version: ${_actual_ver}${_ubuntu_ver}"
+echo -e "PKGBUILD version: ${pkgver}"
 echo -e "Upstream version: ${UPSTREAM_VER}"
 echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
