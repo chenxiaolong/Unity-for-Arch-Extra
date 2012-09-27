1aa4bc8daf55807fbe53859e9834767be1dd8d69
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-27 16:24:27 -0400

    [lightdm-remote-session-freerdp] Added

---
+++ lightdm-remote-session-freerdp/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/lightdm-remote-session-freerdp' -O - | sed -n 's/.*>lightdm-remote-session-freerdp_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q 'https://launchpad.net/lightdm-remote-session-freerdp/+download' -O - | sed -n 's/.*lightdm-remote-session-freerdp-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
