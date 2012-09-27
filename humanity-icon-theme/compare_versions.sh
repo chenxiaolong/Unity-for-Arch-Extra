0386e6c8ef296119db8a0254928c8288e0b1d0b4
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-27 12:48:24 -0400

    [humanity-icon-theme] Update to version 0.6

--- humanity-icon-theme/compare_versions.sh
+++ humanity-icon-theme/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/humanity-icon-theme' -O - | sed -n 's/.*>humanity-icon-theme_\(.*\)\.tar\.gz<.*/\1/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/humanity-icon-theme' -O - | sed -n 's/.*>humanity-icon-theme_\(.*\)\.tar\.gz<.*/\1/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/humanity/+download' -O - | sed -n 's/.*[Hh]umanity-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
