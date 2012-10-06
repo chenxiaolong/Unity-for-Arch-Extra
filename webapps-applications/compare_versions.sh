79df82baa0a7fc200ad0ed223ae2c255f07c306c
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-06 11:12:13 -0400

    [webapps-applications] Update version comparison script

--- webapps-applications/compare_versions.sh
+++ webapps-applications/compare_versions.sh
@@ -2,8 +2,8 @@
 
 source "$(dirname ${0})/PKGBUILD"
 
-#echo "Getting latest Ubuntu version..."
-#UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libunity' -O - | sed -n 's/.*>libunity_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/webapps-applications' -O - | sed -n 's/.*>webapps-applications_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/webapps-applications/+download' -O - | sed -n 's/.*webapps-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
@@ -12,5 +12,4 @@ echo ""
 
 echo -e "PKGBUILD version: ${pkgver}"
 echo -e "Upstream version: ${UPSTREAM_VER}"
-#echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
-echo -e "Ubuntu version:   (none)"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
