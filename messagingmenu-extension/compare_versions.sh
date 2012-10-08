69d08d43d871fb8d52defc9972ebed380daffb12
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-08 16:19:06 -0400

    [messagingmenu-extension] Update version comparison script

--- messagingmenu-extension/compare_versions.sh
+++ messagingmenu-extension/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q -O - 'https://code.launchpad.net/~extension-hackers/messagingmenu-extension/0.8' | grep Releasing | sed -n 's/^.*<p>Releasing\ \(.*\)<\/p>/\1/p' | head -n 1)
+UPSTREAM_VER=$(wget -q -O - 'https://code.launchpad.net/~extension-hackers/messagingmenu-extension/1.0' | grep Releasing | sed -n 's/^.*<p>Releasing\ \(.*\)<\/p>/\1/p' | head -n 1)
 
 echo ""
 
