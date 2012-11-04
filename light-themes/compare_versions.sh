c80a46d6d7af0c80e405c73368fe291f85b7c9f7
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-11-04 15:36:07 -0500

    [light-themes] Update version comparison script

--- light-themes/compare_versions.sh
+++ light-themes/compare_versions.sh
@@ -3,10 +3,10 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/light-themes' -O - | sed -n 's/.*>light-themes_\(.*\)-\(.*\)\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/light-themes' | sed -n 's/^.*current\ release\ (\(.*\)).*$/\1/p'))
 
 echo ""
 
-echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "PKGBUILD version: ${pkgver}"
 echo -e "Upstream version: (none)"
 echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
