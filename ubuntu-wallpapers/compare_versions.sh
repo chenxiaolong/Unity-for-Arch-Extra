62eaf9571eea3b0347c9b79131feccdc21536424
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-08 05:01:28 -0500

    [ubuntu-wallpapers] Update to version 13.04daily12.12.13.1 with Ubuntu release 0ubuntu1

--- ubuntu-wallpapers/compare_versions.sh
+++ ubuntu-wallpapers/compare_versions.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
+source "$(dirname ${0})/../version_checker.sh"
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/ubuntu-wallpapers' -O - | sed -n 's/.*>ubuntu-wallpapers_\(.*\)\.tar\.gz<.*/\1/p'))
-
-echo ""
-
-echo -e "PKGBUILD version: ${pkgver}"
+echo -e "PKGBUILD version: ${_actual_ver}${_extra_ver} ${_ubuntu_rel}"
 echo -e "Upstream version: (none)"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-raring})"
