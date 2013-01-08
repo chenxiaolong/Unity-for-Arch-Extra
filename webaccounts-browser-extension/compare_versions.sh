50b0261f8940addd0bb5705d0208a44b96c7935b
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-08 08:14:09 -0500

    [webaccounts-browser-extension] Update to version 0.4.5 with Ubuntu release 0ubuntu4

--- webaccounts-browser-extension/compare_versions.sh
+++ webaccounts-browser-extension/compare_versions.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
-
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/webaccounts-browser-extension' -O - | sed -n 's/.*>webaccounts-browser-extension_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
-
-#echo "Getting latest upstream version..."
-#UPSTREAM_VER=$(wget -q 'https://launchpad.net/libunity/+download' -O - | sed -n 's/.*libunity-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
-
-echo ""
+source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
-#echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Upstream version: (none)"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "Upstream version: $(get_launchpad_version ${pkgbase})"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgbase} ${1:-raring})"
