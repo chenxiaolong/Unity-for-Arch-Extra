8875fdfecdc22837277e988d25ae5b10b435e2e3
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-08 06:40:56 -0500

    [lo-menubar] Use new version comparison script

--- lo-menubar/compare_versions.sh
+++ lo-menubar/compare_versions.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
-
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/lo-menubar' -O - | sed -n 's/.*>lo-menubar_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/lo-menubar/+download' -O - | sed -n 's/.*lo-menubar-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
-
-echo ""
+source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "spec file version: ${pkgver}"
-echo -e "Upstream version:  ${UPSTREAM_VER}"
-echo -e "Ubuntu version:    ${UBUNTU_VER[@]}"
+echo -e "Upstream version:  $(get_launchpad_version lo-menubar)"
+echo -e "Ubuntu version:    $(get_ubuntu_version lo-menubar ${1:-raring})"
