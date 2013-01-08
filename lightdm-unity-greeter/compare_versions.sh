b10d2c46df314306e453d383c37aa13468924612
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-08 06:03:26 -0500

    [lightdm-unity-greeter] Update to version 13.04.0 with Ubuntu release 0ubuntu1

--- lightdm-unity-greeter/compare_versions.sh
+++ lightdm-unity-greeter/compare_versions.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
-
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/unity-greeter' -O - | sed -n 's/.*>unity-greeter_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/unity-greeter/+download' -O - | sed -n 's/.*unity-greeter-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
-
-echo ""
+source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
-echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "Upstream version: $(get_launchpad_version unity-greeter)"
+echo -e "Ubuntu version:   $(get_ubuntu_version unity-greeter ${1:-raring})"
