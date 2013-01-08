eca4ba7637fea9c1078a4c218bb3d5c4a7979024
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-08 08:45:18 -0500

    [remote-login-service] Update to version 1.0.0 with Ubuntu release 0ubuntu2

--- remote-login-service/compare_versions.sh
+++ remote-login-service/compare_versions.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
-
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/remote-login-service' -O - | sed -n 's/.*>remote-login-service_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/remote-login-service/+download' -O - | sed -n 's/.*remote-login-service-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
-
-echo ""
+source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${pkgver}"
-echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "Upstream version: $(get_launchpad_version ${pkgname})"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-raring})"
