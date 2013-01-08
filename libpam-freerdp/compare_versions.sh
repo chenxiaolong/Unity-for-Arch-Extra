4e4dd6ae62a0b72602b6e4cea8080a219da4fad2
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-08 08:54:28 -0500

    [libpam-freerdp] Use new version comparison script

--- libpam-freerdp/compare_versions.sh
+++ libpam-freerdp/compare_versions.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
-
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libpam-freerdp' -O - | sed -n 's/.*>libpam-freerdp_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/libpam-freerdp/+download' -O - | sed -n 's/.*libpam-freerdp-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
-
-echo ""
+source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${pkgver}"
-echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "Upstream version: $(get_launchpad_version ${pkgname})"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-raring})"
