34b6da9eb7a5301b32b83fdc99f620b3f585616f
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-08 07:14:10 -0500

    [telepathy-indicator] Use new version comparison script

--- telepathy-indicator/compare_versions.sh
+++ telepathy-indicator/compare_versions.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-source PKGBUILD
-
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/telepathy-indicator' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/telepathy-indicator/+download' -O - | sed -n 's/.*telepathy-indicator-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
-
-echo ""
+source "$(dirname ${0})/PKGBUILD"
+source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "spec file version: ${_actual_ver} ${_ubuntu_rel}"
-echo -e "Upstream version:  ${UPSTREAM_VER}"
-echo -e "Ubuntu version:    ${UBUNTU_VER[@]}"
+echo -e "Upstream version:  $(get_launchpad_version ${pkgname})"
+echo -e "Ubuntu version:    $(get_ubuntu_version ${pkgname} ${1:-raring})"
