72869ae429a90b08ef939a27646a2aadb5a885c6
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-08 06:35:41 -0500

    [liferea-indicator] Update to version 1.8.10 with Ubuntu release 0ubuntu1

--- liferea-indicator/compare_versions.sh
+++ liferea-indicator/compare_versions.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
+source "$(dirname ${0})/../version_checker.sh"
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/liferea' -O - | sed -n 's/.*>liferea_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q -O - 'http://sourceforge.net/projects/liferea/files/Liferea%20Stable/' | sed -n 's/^.*Download\ liferea-\(.*\)\.tar\.gz.*$/\1/p')
-
-echo ""
-
-echo -e "PKGBUILD version: ${pkgver}"
+echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
+UPSTREAM_VER=$(wget -q -O - 'http://sourceforge.net/projects/liferea/files/Liferea%20Stable/' | sed -n 's/^.*Download\ liferea-\(.*\)\.tar\..*$/\1/p')
 echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "Ubuntu version:   $(get_ubuntu_version liferea ${1:-raring})"
