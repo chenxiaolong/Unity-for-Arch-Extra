7bf30e48b2b5174bf1c3c5d8cf4928c3bdf0ab0f
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-08 06:39:27 -0500

    [xfce4-indicator-plugin] Use new version comparison script

--- xfce4-indicator-plugin/compare_versions.sh
+++ xfce4-indicator-plugin/compare_versions.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
-
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/xfce4-indicator-plugin' -O - | sed -n 's/.*>xfce4-indicator-plugin_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q "http://goodies.xfce.org/projects/panel-plugins/xfce4-indicator-plugin" -O - | sed -n 's/.*xfce4-indicator-plugin-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
-
-echo ""
+source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${pkgver}"
+UPSTREAM_VER=$(wget -q "http://goodies.xfce.org/projects/panel-plugins/xfce4-indicator-plugin" -O - | sed -n 's/.*xfce4-indicator-plugin-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
 echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-raring})"
