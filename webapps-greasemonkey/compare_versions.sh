107552f506cf873b586ee62f73f42b04dc461c5c
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-08 07:32:14 -0500

    [webapps-greasemonkey] Use new version comparison script

--- webapps-greasemonkey/compare_versions.sh
+++ webapps-greasemonkey/compare_versions.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
-
-#echo "Getting latest Ubuntu version..."
-#UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/webapps-greasemonkey' -O - | sed -n 's/.*>webapps-greasemonkey_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/webapps-greasemonkey/+download' -O - | sed -n 's/.*webapps-greasemonkey-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
-
-echo ""
+source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${pkgver}"
-echo -e "Upstream version: ${UPSTREAM_VER}"
-#echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
-echo -e "Ubuntu version:   (none)"
+echo -e "Upstream version: $(get_launchpad_version webapps-greasemonkey)"
+echo -e "Ubuntu version:   $(get_ubuntu_version webapps-greasemonkey ${1:-raring})"
