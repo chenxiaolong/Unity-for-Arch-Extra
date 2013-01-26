bf578f3bc2e8422bbd379baa880d4c7bb1519ace
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-26 17:32:43 -0500

    [unity-firefox-extension] Fix version comparison script

--- unity-firefox-extension/compare_versions.sh
+++ unity-firefox-extension/compare_versions.sh
@@ -3,6 +3,6 @@
 source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version: ${pkgver}"
+echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
 echo -e "Upstream version: $(get_launchpad_version unity-firefox-extension)"
 echo -e "Ubuntu version:   $(get_ubuntu_version unity-firefox-extension ${1:-raring})"
