b8e71e1aa31399ada047470d1e49dea1f7f55230
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-26 17:43:48 -0500

    [python-pyside] Use new version comparison script

--- python-pyside/compare_versions.sh
+++ python-pyside/compare_versions.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
-
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/pyside' -O - | sed -n 's/.*>pyside_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'http://www.pyside.org/files/' -O - | sed -n 's/.*>pyside-qt4.8+\(.*\)\.tar\.bz2<.*/\1/p' | grep -v latest | tail -1)
-
-echo ""
+source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${pkgver}"
+UPSTREAM_VER=$(wget -q 'https://distfiles.macports.org/py-pyside/' -O - | sed -n 's/.*>pyside-qt4.8+\(.*\)\.tar\.bz2<.*/\1/p' | grep -v latest | tail -1)
 echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "Ubuntu version:   $(get_ubuntu_version pyside ${1:-raring})"
