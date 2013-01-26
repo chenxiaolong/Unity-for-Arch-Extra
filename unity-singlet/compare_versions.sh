77a5ac0e44e77a8558dea165a1df5e4a726ce3b4
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-26 17:46:57 -0500

    [unity-singlet] Use new version comparison script

--- unity-singlet/compare_versions.sh
+++ unity-singlet/compare_versions.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
-
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/unity-singlet' -O - | sed -n 's/.*>unity-singlet_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/singlet/+download' -O - | sed -n 's/.*unity-singlet_\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
-
-echo ""
+source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${pkgver}"
-echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "Upstream version: $(get_launchpad_version singlet unity-singlet)"
+echo -e "Ubuntu version:   $(get_ubuntu_version unity-singlet ${1:-raring})"
