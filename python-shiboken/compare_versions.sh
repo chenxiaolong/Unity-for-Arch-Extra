214dce694ea4a7e5f5c3775f6b0c9ce4c3b33af4
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-26 17:42:15 -0500

    [python-shiboken] Use new version comparison script

--- python-shiboken/compare_versions.sh
+++ python-shiboken/compare_versions.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
-
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/shiboken' -O - | sed -n 's/.*>shiboken_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'http://www.pyside.org/files/' -O - | sed -n 's/.*>shiboken-\(.*\)\.tar\.bz2<.*/\1/p' | grep -v latest | tail -1)
-
-echo ""
+source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${pkgver}"
+UPSTREAM_VER=$(wget -q 'https://distfiles.macports.org/py-shiboken/' -O - | sed -n 's/.*>shiboken-\(.*\)\.tar\.bz2<.*/\1/p' | grep -v latest | tail -1)
 echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "Ubuntu version:   $(get_ubuntu_version shiboken ${1:-raring})"
