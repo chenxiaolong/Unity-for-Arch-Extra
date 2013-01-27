8229e02373786213ba608d11480ca318843f0eb2
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-27 00:33:19 -0500

    [python2-magic] Use new version comparison script

--- python2-magic/compare_versions.sh
+++ python2-magic/compare_versions.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
-
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/file' -O - | sed -n 's/.*>file_\(.*\)-\(.*\)\.debian\.tar\.xz<.*/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'ftp://ftp.astron.com/pub/file/' -O - | sed -n 's/.*>file-\(.*\)\.tar\.gz<.*/\1/p' | tail -1)
-
-echo ""
+source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${pkgver}"
+UPSTREAM_VER=$(wget -q 'ftp://ftp.astron.com/pub/file/' -O - | sed -n 's/.*>file-\(.*\)\.tar\.gz<.*/\1/p' | tail -1)
 echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "Ubuntu version:   $(get_ubuntu_version file ${1:-raring})"
