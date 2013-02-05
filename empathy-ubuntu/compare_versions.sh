4ed7ea8918f96acf084400d21fba2653fa5d23e9
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-02-04 19:32:45 -0500

    [empathy-ubuntu] Update to version 3.6.3 with Ubuntu release 0ubuntu2

--- empathy-ubuntu/compare_versions.sh
+++ empathy-ubuntu/compare_versions.sh
@@ -3,6 +3,6 @@
 source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version: ${_actual_ver} ${_ubuntu_rel}"
+echo -e "PKGBUILD version: ${pkgver} ${_ubuntu_rel}"
 echo -e "Upstream version: $(get_gnome_version ${pkgname%-*} 3.6)"
 echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname%-*} ${1:-raring})"
