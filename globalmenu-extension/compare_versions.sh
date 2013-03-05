378f4dfa2ed57a9222f3f66cb050f183d8945571
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-03-05 18:27:40 -0500

    [globalmenu-extension] Update to version 3.7.2

--- globalmenu-extension/compare_versions.sh
+++ globalmenu-extension/compare_versions.sh
@@ -2,11 +2,7 @@
 
 source "$(dirname ${0})/PKGBUILD"
 
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q -O - 'https://code.launchpad.net/~extension-hackers/globalmenu-extension/3.7' | grep Releasing | sed -n 's/^.*<p>Releasing\ \(.*\)<\/p>/\1/p' | head -n 1)
-
-echo ""
-
 echo -e "PKGBUILD version: ${pkgver}"
+UPSTREAM_VER=$(wget -q -O - 'https://code.launchpad.net/~extension-hackers/globalmenu-extension/3.7' | grep Releasing | sed -n 's/^.*<p>Releasing\ \(.*\)<\/p>/\1/p' | head -n 1)
 echo -e "Upstream version: ${UPSTREAM_VER}"
 echo -e "Ubuntu version:   (n/a)"
