55d66b2bf4b5c5163de77750494654fa9ebd6ccf
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-04-16 17:59:35 -0400

    [messagingmenu-extension] Update version comparison script

--- messagingmenu-extension/compare_versions.sh
+++ messagingmenu-extension/compare_versions.sh
@@ -2,11 +2,7 @@
 
 source "$(dirname ${0})/PKGBUILD"
 
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q -O - 'https://code.launchpad.net/~extension-hackers/messagingmenu-extension/1.0' | grep Releasing | sed -n 's/^.*<p>Releasing\ \(.*\)<\/p>/\1/p' | head -n 1)
-
-echo ""
-
 echo -e "PKGBUILD version: ${pkgver}"
+UPSTREAM_VER=$(wget -q -O - 'https://code.launchpad.net/~extension-hackers/messagingmenu-extension/1.0' | grep Releasing | sed -n 's/^.*<p>Releasing\ \(.*\)<\/p>/\1/p' | head -n 1)
 echo -e "Upstream version: ${UPSTREAM_VER}"
 echo -e "Ubuntu version:   (n/a)"
