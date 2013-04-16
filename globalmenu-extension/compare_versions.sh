9d9040ae9583019e63cf178773c015bf8fedc0d8
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-04-16 17:58:12 -0400

    [globalmenu-extension] Update version comparison script

--- globalmenu-extension/compare_versions.sh
+++ globalmenu-extension/compare_versions.sh
@@ -3,6 +3,6 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo -e "PKGBUILD version: ${pkgver}"
-UPSTREAM_VER=$(wget -q -O - 'https://code.launchpad.net/~extension-hackers/globalmenu-extension/3.7' | grep Releasing | sed -n 's/^.*<p>Releasing\ \(.*\)<\/p>/\1/p' | head -n 1)
+UPSTREAM_VER=$(wget -q -O - 'https://code.launchpad.net/~extension-hackers/globalmenu-extension' | grep Releasing | sed -n 's/^.*<p>Releasing\ \(.*\)<\/p>/\1/p' | head -n 1)
 echo -e "Upstream version: ${UPSTREAM_VER}"
 echo -e "Ubuntu version:   (n/a)"
