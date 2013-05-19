842899b23cc596e021c9fdb213ad26d66cafad08
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-05-19 17:27:17 -0400

    [lo-menubar] Removed: No longer required for global menubar support

--- lo-menubar/compare_versions.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname ${0})/PKGBUILD"
-source "$(dirname ${0})/../version_checker.sh"
-
-echo -e "spec file version: ${pkgver}"
-echo -e "Upstream version:  $(get_launchpad_version lo-menubar)"
-echo -e "Ubuntu version:    $(get_ubuntu_version lo-menubar ${1:-raring})"
