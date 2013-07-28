e4a3eb657e1e30281c31df1b4599ac49906e0033
chenxiaolong <chenxiaolong@master>
2013-07-28 14:57:57 -0400

    [globalmenu-extension] Removed

--- globalmenu-extension/compare_versions.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname ${0})/PKGBUILD"
-
-echo -e "PKGBUILD version: ${pkgver}"
-UPSTREAM_VER=$(wget -q -O - 'https://code.launchpad.net/~extension-hackers/globalmenu-extension' | grep Releasing | sed -n 's/^.*<p>Releasing\ \(.*\)<\/p>/\1/p' | head -n 1)
-echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   (n/a)"
