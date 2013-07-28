2b52bd012976440c5325b050294f4ba2f2e96ff0
chenxiaolong <chenxiaolong@master>
2013-07-28 15:14:44 -0400

    [liferea-indicator] Update to version 1.8.15 with Ubuntu release 1ubuntu1

--- liferea-indicator/compare_versions.sh
+++ liferea-indicator/compare_versions.sh
@@ -6,4 +6,4 @@ source "$(dirname ${0})/../version_checker.sh"
 echo -e "PKGBUILD version: ${pkgver} ${_ubuntu_rel}"
 UPSTREAM_VER=$(wget -q -O - 'http://sourceforge.net/projects/liferea/files/Liferea%20Stable/' | sed -n 's/^.*Download\ liferea-\(.*\)\.tar\..*$/\1/p')
 echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   $(get_ubuntu_version liferea ${1:-raring})"
+echo -e "Ubuntu version:   $(get_ubuntu_version liferea ${1:-saucy})"
