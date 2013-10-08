c0b38baa1cb7af36a1f917d699fa0136b5804fac
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-10-08 19:20:24 -0400

    [python-polib] Removed: No dependencies on package

--- python-polib/compare_versions.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname ${0})/PKGBUILD"
-source "$(dirname ${0})/../version_checker.sh"
-
-echo -e "PKGBUILD version: ${pkgver}"
-UPSTREAM_VER=$(wget -q 'https://bitbucket.org/izi/polib/downloads' -O - | sed -n 's/.*>polib-\(.*\)\.tar\.gz<.*/\1/p' | head -n 1)
-echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   $(get_ubuntu_version polib ${1:-saucy})"
