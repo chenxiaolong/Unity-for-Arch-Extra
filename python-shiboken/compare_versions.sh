2fe7470bd62ddfc4293df98f9ad16aee19ab1631
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-12-17 23:37:40 -0500

    [python-shiboken] Update to version 1.2.1

--- python-shiboken/compare_versions.sh
+++ python-shiboken/compare_versions.sh
@@ -6,4 +6,4 @@ source "$(dirname ${0})/../version_checker.sh"
 echo -e "PKGBUILD version: ${pkgver}"
 UPSTREAM_VER=$(wget -q 'https://distfiles.macports.org/py-shiboken/' -O - | sed -n 's/.*>shiboken-\(.*\)\.tar\.bz2<.*/\1/p' | grep -v latest | tail -1)
 echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   $(get_ubuntu_version shiboken ${1:-saucy})"
+echo -e "Ubuntu version:   $(get_ubuntu_version shiboken ${1:-trusty})"
