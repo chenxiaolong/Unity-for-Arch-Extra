a64cd44749595583e2e4cc0daa6442102f30d95e
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-12-17 23:39:42 -0500

    [python2-magic] Update to version 5.16

--- python2-magic/compare_versions.sh
+++ python2-magic/compare_versions.sh
@@ -6,4 +6,4 @@ source "$(dirname ${0})/../version_checker.sh"
 echo -e "PKGBUILD version: ${pkgver}"
 UPSTREAM_VER=$(wget -q 'ftp://ftp.astron.com/pub/file/' -O - | sed -n 's/.*>file-\(.*\)\.tar\.gz<.*/\1/p' | tail -1)
 echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   $(get_ubuntu_version file ${1:-saucy})"
+echo -e "Ubuntu version:   $(get_ubuntu_version file ${1:-trusty})"
