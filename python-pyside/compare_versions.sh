eaca52a439f0fc8cc1747bead2f9d245d953a1ad
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-12-20 13:18:43 -0500

    [python-pyside] Update to version 1.2.1

--- python-pyside/compare_versions.sh
+++ python-pyside/compare_versions.sh
@@ -6,4 +6,4 @@ source "$(dirname ${0})/../version_checker.sh"
 echo -e "PKGBUILD version: ${pkgver}"
 UPSTREAM_VER=$(wget -q 'https://distfiles.macports.org/py-pyside/' -O - | sed -n 's/.*>pyside-qt4.8+\(.*\)\.tar\.bz2<.*/\1/p' | grep -v latest | tail -1)
 echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   $(get_ubuntu_version pyside ${1:-saucy})"
+echo -e "Ubuntu version:   $(get_ubuntu_version pyside ${1:-trusty})"
