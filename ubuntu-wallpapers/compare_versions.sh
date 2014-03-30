e46351215f7cb71ef9e90dc0eff9bb47a0d809d0
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-03-30 17:17:14 -0400

    [ubuntu-wallpapers] Update to version 14.04.0.1 with Ubuntu release 0ubuntu1

--- ubuntu-wallpapers/compare_versions.sh
+++ ubuntu-wallpapers/compare_versions.sh
@@ -3,6 +3,6 @@
 source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version: ${_actual_ver}${_extra_ver} ${_ubuntu_rel}"
+echo -e "PKGBUILD version: ${pkgver} ${_ubuntu_rel}"
 echo -e "Upstream version: (none)"
 echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-trusty})"
