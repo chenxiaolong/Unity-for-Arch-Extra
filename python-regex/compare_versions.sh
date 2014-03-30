79834606be70f237d625f41a6d290c615d140ca7
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-03-30 17:46:16 -0400

    [python-regex] Update to version 0.1.20140216

--- python-regex/compare_versions.sh
+++ python-regex/compare_versions.sh
@@ -5,5 +5,5 @@ source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${pkgver}"
 echo -e "Upstream version: (none)"
-echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgbase} ${1:-saucy})"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgbase} ${1:-trusty})"
 echo -e "Debian version:   TODO"
