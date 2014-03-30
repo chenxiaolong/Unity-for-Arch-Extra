c361776bef4face25b32c25c6cc5fa43a10278fe
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-03-30 17:26:53 -0400

    [xfce4-indicator-plugin] Track trusty repos

--- xfce4-indicator-plugin/compare_versions.sh
+++ xfce4-indicator-plugin/compare_versions.sh
@@ -6,4 +6,4 @@ source "$(dirname ${0})/../version_checker.sh"
 echo -e "PKGBUILD version: ${pkgver}"
 UPSTREAM_VER=$(wget -q "http://goodies.xfce.org/projects/panel-plugins/xfce4-indicator-plugin" -O - | sed -n 's/.*xfce4-indicator-plugin-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
 echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-saucy})"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-trusty})"
