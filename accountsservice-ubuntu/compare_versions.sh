25d5a1a7222eccb082659e08327c905b30cbcf55
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-04-10 22:21:37 -0400

    [accountsservice-ubuntu] Update to version 0.6.37 with Ubuntu version 0.6.35 and release 0ubuntu7

--- accountsservice-ubuntu/compare_versions.sh
+++ accountsservice-ubuntu/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version:   ${pkgver} ${_ubuntu_rel}"
+echo -e "PKGBUILD version:   ${pkgver} Ubuntu ${_ubuntu_ver} ${_ubuntu_rel}"
 echo -e "Upstream version:   $(get_freedesktop_version ${pkgname%-*})"
 echo -e "Arch Linux version: $(get_archlinux_version ${pkgname%-*} extra x86_64)"
 echo -e "Ubuntu version:     $(get_ubuntu_version ${pkgname%-*} ${1:-trusty})"
